fc8249a0727aff7fb26905eb306f25a1feda02a9
shresth <shrestha.tripathi@gmail.com>
2024-01-07 13:34:58 +0530

    9:04 chat socket init

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,6 +1,7 @@
 import { Hash } from "lucide-react";
 import { MobileToggle } from "../mobile-toggle";
 import { UserAvatar } from "../user-avatar";
+import { SocketIndicator } from "@/components/socket-indicator";
 
 
 interface ChatHeaderProps {
@@ -31,6 +32,9 @@ export const ChatHeader = ({
             <p className="font-semibold text-md text-black dark:text-white">
                 {name}
             </p>
+            <div className="ml-auto flex items-center">
+                <SocketIndicator />
+            </div>
         </div>
     );
 }
